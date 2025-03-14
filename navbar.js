5347485dd4b6ab5df368df3c782f8c609f3558a5
merd <mertcanyilmaz5420@gmail.com>
2025-03-14 20:24:40 +0300

    bugfix 7

--- navbar.js
+++ navbar.js
@@ -1,8 +1,27 @@
-document.querySelector("#menu-btn").addEventListener("click", function(){
+/* document.querySelector("#menu-btn").addEventListener("click", function(){
     var navbar = document.querySelector("#navbar");
     if (navbar.style.right === "0px") {
         navbar.style.right = "-250px";
     } else {
         navbar.style.right = "0px";
     }
-})
+}) */
+document.querySelector("#menu-btn").addEventListener("click", function (event) {
+    var navbar = document.querySelector("#navbar");
+    if (navbar.style.right === "0px") {
+        navbar.style.right = "-250px";
+    } else {
+        navbar.style.right = "0px";
+    }
+    event.stopPropagation(); // Menü düğmesine tıklandığında olayın yukarı yayılmasını engeller
+});
+
+document.addEventListener("click", function (event) {
+    var navbar = document.querySelector("#navbar");
+    var menuBtn = document.querySelector("#menu-btn");
+
+    // Tıklanan öğe navbar veya menuBtn değilse, menüyü kapat
+    if (navbar.style.right === "0px" && !navbar.contains(event.target) && !menuBtn.contains(event.target)) {
+        navbar.style.right = "-250px";
+    }
+});
