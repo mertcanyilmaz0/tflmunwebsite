23dd117c3cc690d520963cdf4d865c4cf2492d1f
merd <mertcanyilmaz5420@gmail.com>
2025-03-16 19:25:19 +0300

    Apply bitti

--- js/navbar.js
+++ js/navbar.js
@@ -1,27 +1,76 @@
-/* document.querySelector("#menu-btn").addEventListener("click", function(){
-    var navbar = document.querySelector("#navbar");
-    if (navbar.style.right === "0px") {
-        navbar.style.right = "-250px";
-    } else {
-        navbar.style.right = "0px";
-    }
-}) */
 document.querySelector("#menu-btn").addEventListener("click", function (event) {
     var navbar = document.querySelector("#navbar");
+    var dropDownNavbar = document.querySelector("#dropdownNav");
     if (navbar.style.right === "0px") {
         navbar.style.right = "-250px";
+        dropDownNavbar.style.visibility = 'hidden';
+        dropDownNavbar.style.opacity = "0";
+        console.log("görünmez");
+        dropDown.style.borderBottom = '';
+        dropDown.style.paddingBottom = '';
+        dropDown.style.display = '';
     } else {
         navbar.style.right = "0px";
     }
-    event.stopPropagation(); // Menü düğmesine tıklandığında olayın yukarı yayılmasını engeller
+    event.stopPropagation();
 });
 
 document.addEventListener("click", function (event) {
     var navbar = document.querySelector("#navbar");
     var menuBtn = document.querySelector("#menu-btn");
 
-    // Tıklanan öğe navbar veya menuBtn değilse, menüyü kapat
+
     if (navbar.style.right === "0px" && !navbar.contains(event.target) && !menuBtn.contains(event.target)) {
         navbar.style.right = "-250px";
+    }
+});
+document.querySelector("#dropdown").addEventListener("click", function(event) {
+    var dropDownNavbar = document.querySelector("#dropdownNav");
+    var dropDown = document.querySelector("#dropdown");
+
+    var screenWidth = window.innerWidth;
+    if (screenWidth <= 991) {
+        if (dropDownNavbar.style.opacity === "0" || dropDownNavbar.style.opacity === "") {
+            dropDownNavbar.style.visibility = 'visible';
+            dropDownNavbar.style.opacity = "1";
+            dropDown.style.borderBottom = '0.1rem solid var(--bg-color)';
+            dropDown.style.paddingBottom = '1rem';
+            dropDown.style.display = 'flex';
+            console.log("görünür");
+        } else {
+            dropDownNavbar.style.visibility = 'hidden';
+            dropDownNavbar.style.opacity = "0";
+            console.log("görünmez");
+            dropDown.style.borderBottom = '';
+            dropDown.style.paddingBottom = '';
+            dropDown.style.display = '';
+        }
+    } else {
+
+        if (dropDownNavbar.style.opacity === "0" || dropDownNavbar.style.opacity === "") {
+            dropDownNavbar.style.visibility = 'visible';
+            dropDownNavbar.style.opacity = "1";
+            dropDown.style.borderBottom = '0.1rem solid var(--bg-color)';
+            dropDown.style.paddingBottom = '1rem';
+            dropDown.style.display = 'flex';
+            console.log("görünür");
+        } else {
+            dropDownNavbar.style.visibility = 'hidden';
+            dropDownNavbar.style.opacity = "0";
+            dropDown.style.borderBottom = '';
+            dropDown.style.paddingBottom = '';
+            dropDown.style.display = '';
+            console.log("görünmez");
+        }
+    }
+});
+window.addEventListener("resize", function() {
+    var dropDownNavbar = document.querySelector("#dropdownNav");
+    var screenWidth = window.innerWidth;
+
+    if (screenWidth <= 991) {
+        dropDownNavbar.style.visibility = 'hidden';
+        dropDownNavbar.style.opacity = "0";
+        console.log("görünmez");
     }
 });
